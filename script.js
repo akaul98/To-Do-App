5cffd1cca5a49c310e9d8751a21a628414bc78a6
Adarsh Kaul <ak.adarshkaul@gmail.com>
2023-06-17 02:32:27 +0530

    CRUD completed

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ function onClickAdd(){
       let li=document.createElement("li")
       li.innerHTML=task.value
       list.appendChild(li)
+      let span=document.createElement("span")
+      span.innerHTML="\u00d7"
+      li.appendChild(span)
+
 }
 task.value=""
 }
@@ -17,7 +21,16 @@ task.value=""
 function add(e){
   if(e.keyCode === 13){
       e.preventDefault(); // Ensure it is only this code that runs
-      console.log(e.preventDefault())
       this.onClickAdd()
   }
 }
+
+list.addEventListener("click",function(e){
+  console.log("hi line 30",e.target)
+if (e.target.tagName==="LI"){
+  e.target.classList.toggle("checked")
+}
+else if(e.target.tagName==="SPAN"){
+  e.target.parentElement.remove();
+} 
+},false)
